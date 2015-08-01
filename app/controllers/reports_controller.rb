bef93e8b90bee819c9d8874b22a34fb39addba3f
Abhishek Bose <abhishekbose87@gmail.com>
2015-08-01 19:14:16 +0530

    Use generic query to fetch report data

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,21 @@
 class ReportsController < ApplicationController
 
   def index
+
     PomodoroUnit.joins(:project).group("pomodoro_units.work_date").select(
-      "count(CASE WHEN projects.name = 'Mesitis' THEN 1 END) as mesitis,
-       count(CASE WHEN projects.name = 'Learning' THEN 1 END) as learning,
-       count(CASE WHEN projects.name = 'Others' THEN 1 END) as others,
-       count(CASE WHEN projects.name = 'Clicksome' THEN 1 END) as clicksome,
+      "#{case_query},
        pomodoro_units.work_date as date"
       ).order("date asc").to_jsvar(:pomodoro_units)
+  end
+
+  private
+
+  def case_query
+    case_statements_array = []
+    Project.all.each do |project|
+      case_statements_array << "count(CASE WHEN projects.name = '#{project.name}' THEN 1 END) as #{project.name}"
+    end
+    case_statements_array.join(", ")
   end
 
 end
