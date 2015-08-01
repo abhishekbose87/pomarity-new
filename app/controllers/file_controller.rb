f2a1fc831a7a0a316c8c223ba025ed82d44c7efc
Abhishek Bose <abhishekbose87@gmail.com>
2015-08-01 19:57:07 +0530

    Allow only correct files to be processed and handle errors during creating data

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -6,23 +6,28 @@ class FileController < ApplicationController
 
   def pomodoro_upload
     if params["pomodoro_upload_file"].present? && simple_captcha_valid?
-      uploaded_io = params["pomodoro_upload_file"]
-      file_path = Rails.root.join('public', 'uploads', 'pomodoro_upload_file.json')
+      begin
+        raise exception unless params["pomodoro_upload_file"].content_type == 'application/octet-stream'
+        uploaded_io = params["pomodoro_upload_file"]
+        file_path = Rails.root.join('public', 'uploads', 'pomodoro_upload_file.json')
 
-      File.open(file_path, 'wb') do |file|
-        file.write(uploaded_io.read)
+        File.open(file_path, 'wb') do |file|
+          file.write(uploaded_io.read)
+        end
+
+        file = File.read(file_path)
+        data_hash = JSON.parse(file)
+
+        remove_projects
+        remove_pomodoro_units
+
+        load_projects(data_hash["projects"])
+        load_pomodoro_units(data_hash["workUnits"])
+
+        redirect_to root_path, notice: "Data successfully uploaded"
+      rescue
+        redirect_to upload_form_path, notice: "File type not correct"
       end
-
-      file = File.read(file_path)
-      data_hash = JSON.parse(file)
-
-      remove_projects
-      remove_pomodoro_units
-
-      load_projects(data_hash["projects"])
-      load_pomodoro_units(data_hash["workUnits"])
-
-      redirect_to root_path, notice: "Data successfully uploaded"
     else
       redirect_to upload_form_path, notice: "Please fill form correctly"
     end
